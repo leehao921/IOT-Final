89161eb21cd9fa6b014cc1a566a8deb112a8bb25
Ian60509 <ian.60509@gmail.com>
2022-01-20 11:25:58 +0800

    【new】frontend: add polyline

--- backend/firebase_pack/public/google_map.js
+++ backend/firebase_pack/public/google_map.js
@@ -1,25 +1,19 @@
-      // In the following example, markers appear when the user clicks on the map.
-      // Each marker is labeled with a single alphabetical character.
-      // import {GPS_posi} from  "firebase_data.js";
- 
-      
+
+
+
       function initMap() {
 
         const Hsinchu = { lat: 24.7961, lng: 120.9967 };
+        const infoWindow = new google.maps.InfoWindow();
+        var path =[];
+        path.push({ lat: 24.7961, lng: 120.9967 });
+        console.log("path=",path);
 
         const map = new google.maps.Map(document.getElementById("map"), {
-          zoom: 12,
+          zoom: 2,
           center: Hsinchu,
         });
-        addMarker(Hsinchu, map);
-
-
-        // for(var i=0; i<100; i++){
-
-        //     addMarker(tmp, map);
-        // }
-        // // This event listener calls addMarker() when the map is clicked.
-   
+        // addMarker(Hsinchu, map, 3, infoWindow, "123");
 
         // Add a marker at the center of the map.
           var db = firebase.firestore();
@@ -31,28 +25,80 @@
               var lat_data=snap.data().lat
               var lon_data=snap.data().lon
               var GPS_posi={lat:lat_data,lng:lon_data}
-              var tmp = GPS_posi;
-              console.log("GPS add +",tmp)
-              // addMarker(tmp, map);
+              var timestamp = snap.data().timestamp
+              
+              addMarker(GPS_posi, map, 3, infoWindow, timestamp);
               //目前暫時關掉，因為怕流量太大
+              path.push(GPS_posi);
               
             })
         }, (error) => {
             console.log(error.message)
         });
         
-      }
+        //*****************  add a polyline  ******************
+        path.push({lat: 37.772, lng: -122.214 });
 
+        var poly = new google.maps.Polyline({
+           path: path,
+           geodesic: true,
+           strokeColor: "#FFFFFF",
+           strokeOpacity: 0.5,
+           strokeWeight: 2,
+         });
+        
+
+        poly.setMap(map);
+      }
+      
       // Adds a marker to the map.
-      function addMarker(location, map) {
-        const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        let labelIndex = 0;
-        // Add the marker at the clicked location, and add the next-available label
-        // from the array of alphabetical characters.
-        new google.maps.Marker({
+      function addMarker(location, map, repetition, infoWindow, text) {
+        const white = {
+            path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+            fillColor: "#ffffff",
+            fillOpacity: 0.6,
+            strokeWeight: 0,
+            rotation: 0,
+            scale: 2,
+            anchor: new google.maps.Point(15, 30),
+          };
+        const yellow = {
+            path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+            fillColor: "yellow",
+            fillOpacity: 0.6,
+            strokeWeight: 0,
+            rotation: 0,
+            scale: 2,
+            anchor: new google.maps.Point(15, 30),
+          };
+        const red = {
+            path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+            fillColor: "red",
+            fillOpacity: 0.6,
+            strokeWeight: 0,
+            rotation: 0,
+            scale: 2,
+            anchor: new google.maps.Point(15, 30),
+          };
+        var svgMarker = white;
+
+        if(repetition<=1) svgMarker = white;
+        else if(repetition<=5 ) svgMarker = yellow;
+        else svgMarker = red;
+
+        const marker = new google.maps.Marker({
           position: location,
-          label: labels[labelIndex++ % labels.length],
+          // label: "fuck",
           map: map,
+          icon: svgMarker,
+          title: text,
         });
+
+        marker.addListener("click", () => {
+          infoWindow.close();
+          infoWindow.setContent(marker.getTitle());
+          infoWindow.open(marker.getMap(), marker);
+        });
+
       }
 
